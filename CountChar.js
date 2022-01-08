b9a00d0a859de8fbfff03b09675725753a0c6400
Dilara Valitova <87713511+valitovaa@users.noreply.github.com>
2022-01-08 20:41:14 +0500

    Add files via upload

---
+++ CountChar.js
@@ -0,0 +1,39 @@
+//1) построение треугольника
+
+let count =0;
+let abc = "#";
+for(let i=0; i<7; i++){
+  console.log(abc);
+  abc += "#"
+  }
+
+//2) FizzBuzz
+
+for(let i=0; i<100;i++){
+  if(i%3 == 0){
+    console.log("Fizz");
+  }else if(i%5 ==0){
+    console.log("Buzz")
+  }else if(i%3==0 && i%5 ==0){
+    console.log("FizzBuzz")
+  }else
+  console.log(i);
+ }
+
+
+//3) шахматная доска
+
+let size = 8;
+let board = "";
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
+
