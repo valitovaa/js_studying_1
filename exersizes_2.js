d868cdb4ef2bc64b3baac71ea6670f8c934e4419
Dilara Valitova <87713511+valitovaa@users.noreply.github.com>
2022-02-24 23:39:22 +0500

    Update exersizes_2.js

--- exersizes_2.js
+++ exersizes_2.js
@@ -10,12 +10,12 @@ for(let i=0; i<7; i++){
 //2) FizzBuzz
 
 for(let i=0; i<100;i++){
-  if(i%3 == 0){
-    console.log("Fizz");
+  if(i%3==0 && i%5 ==0){
+    console.log("FizzBuzz");
   }else if(i%5 ==0){
     console.log("Buzz")
-  }else if(i%3==0 && i%5 ==0){
-    console.log("FizzBuzz")
+  }else if(i%3==0){
+    console.log("Fizz")
   }else
   console.log(i);
  }
